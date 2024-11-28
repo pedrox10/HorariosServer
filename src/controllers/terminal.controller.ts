751b2071cb66bf4ed01a23aed0d55a9deb5a2935
pedrox10 <barcocarrasco.pedro@gmail.com>
2024-11-27 23:37:01 -0400

    Cambios en server 28/11

--- src/controllers/terminal.controller.ts
+++ src/controllers/terminal.controller.ts
@@ -66,6 +66,15 @@ export const sincronizarTerminal = async (req: Request, res: Response) => {
             }
         }
         await getMarcacionesPy();
+       /* try {
+            const jsonString = fs.readFileSync("./src/registros.json");
+            await JSON.parse(jsonString.toString()).forEach((value: any) => {
+                nuevaMarcacion(terminal, value)
+            });
+        } catch (err) {
+            console.log(err);
+            return;
+        }*/
 
         const getUsuariosPy = async () => {
             try {
@@ -82,6 +91,10 @@ export const sincronizarTerminal = async (req: Request, res: Response) => {
             }
         }
         await getUsuariosPy();
+        /*let aux = [{"uid":1,"role":0,"password":"","name":"PEDRO DINO","cardno":0,"user_id":"9420724"},{"uid":12,"role":0,"password":"111","name":"testing","cardno":0,"user_id":"9"},{"uid":4,"role":0,"password":"0895","name":"","cardno":0,"user_id":"5297992"},{"uid":7,"role":0,"password":"","name":"RUTH  PEREZ CUBA","cardno":0,"user_id":"7948392"},{"uid":8,"role":14,"password":"","name":"","cardno":0,"user_id":"7912911"},{"uid":10,"role":0,"password":"","name":"CARMELO VALENCIA CARBALL","cardno":0,"user_id":"5317614"},{"uid":13,"role":0,"password":"","name":"LOURDES MAITA VELIZ ","cardno":0,"user_id":"14850113"},{"uid":18,"role":0,"password":"","name":"NELIA  LOPEZ AREVALO","cardno":0,"user_id":"13658745"},{"uid":19,"role":0,"password":"","name":"ANDREA  SERRUDO VILLCA","cardno":0,"user_id":"12556096"},{"uid":20,"role":14,"password":"","name":"LUIS","cardno":0,"user_id":"9413936"},{"uid":21,"role":0,"password":"","name":"DENIS FLORES  ARGOTE","cardno":0,"user_id":"6493074"},{"uid":22,"role":14,"password":"","name":"DENILSON","cardno":0,"user_id":"1"}]
+        await aux.forEach((value: any) => {
+            nuevoUsuario(terminal, value)
+        });*/
         res.send({"res": "Listo sincronizado"})
     } else {
         res.send({"res": changeTimezone(terminal?.ult_sincronizacion, "America/La_Paz").toString()})
