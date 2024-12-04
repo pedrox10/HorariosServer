ebd49fd54338bd66862dd722a9c696aaca7729d0
pedrox10 <barcocarrasco.pedro@gmail.com>
2024-12-03 21:41:34 -0400

    Antes de pull

--- src/controllers/terminal.controller.ts
+++ src/controllers/terminal.controller.ts
@@ -53,7 +53,7 @@ export const sincronizarTerminal = async (req: Request, res: Response) => {
     const {id} = req.params;
     const terminal = await Terminal.findOne({where: {id: parseInt(id)},});
     if (terminal?.ult_sincronizacion === null) {
-        const getMarcacionesPy = async () => {
+        /*const getMarcacionesPy = async () => {
             try {
                 const pyFile = 'src/scriptpy/marcaciones.py';
                 const args = [terminal.ip, terminal.puerto];
@@ -74,9 +74,9 @@ export const sincronizarTerminal = async (req: Request, res: Response) => {
                 console.log(e.stderr.toString())
             }
         }
-        await getMarcacionesPy();
+        await getMarcacionesPy();*/
 
-        /*try {
+        try {
             const jsonString = fs.readFileSync("./src/registros.json");
             let marcaciones: Marcacion[] = [];
             await JSON.parse(jsonString.toString()).forEach((value: any) => {
@@ -91,9 +91,9 @@ export const sincronizarTerminal = async (req: Request, res: Response) => {
         } catch (err) {
             console.log(err);
             return;
-        }*/
+        }
 
-        const getUsuariosPy = async () => {
+       /* const getUsuariosPy = async () => {
             try {
                 const pyFile = 'src/scriptpy/usuarios.py';
                 const args = [terminal.ip, terminal.puerto];
@@ -117,8 +117,8 @@ export const sincronizarTerminal = async (req: Request, res: Response) => {
                 console.log(e.stderr.toString())
             }
         }
-        await getUsuariosPy();
-        /*let usuarios: Usuario[]= [];
+        await getUsuariosPy();*/
+        let usuarios: Usuario[]= [];
         let aux = [{"uid":1,"role":0,"password":"","name":"PEDRO DINO","cardno":0,"user_id":"9420724"},{"uid":12,"role":0,"password":"111","name":"testing","cardno":0,"user_id":"9"},{"uid":4,"role":0,"password":"0895","name":"","cardno":0,"user_id":"5297992"},{"uid":7,"role":0,"password":"","name":"RUTH  PEREZ CUBA","cardno":0,"user_id":"7948392"},{"uid":8,"role":14,"password":"","name":"","cardno":0,"user_id":"7912911"},{"uid":10,"role":0,"password":"","name":"CARMELO VALENCIA CARBALL","cardno":0,"user_id":"5317614"},{"uid":13,"role":0,"password":"","name":"LOURDES MAITA VELIZ ","cardno":0,"user_id":"14850113"},{"uid":18,"role":0,"password":"","name":"NELIA  LOPEZ AREVALO","cardno":0,"user_id":"13658745"},{"uid":19,"role":0,"password":"","name":"ANDREA  SERRUDO VILLCA","cardno":0,"user_id":"12556096"},{"uid":20,"role":14,"password":"","name":"LUIS","cardno":0,"user_id":"9413936"},{"uid":21,"role":0,"password":"","name":"DENIS FLORES  ARGOTE","cardno":0,"user_id":"6493074"},{"uid":22,"role":14,"password":"","name":"DENILSON","cardno":0,"user_id":"1"}]
         aux.forEach((value: any) => {
             let usuario = new Usuario();
@@ -130,7 +130,7 @@ export const sincronizarTerminal = async (req: Request, res: Response) => {
         });
         const userRepo = AppDataSource.getRepository(Usuario);
         await userRepo.insert(usuarios);
-        await res.send(usuarios)*/
+        await res.send(usuarios)
     } else {
         res.send(moment(terminal?.ult_sincronizacion).utc(true).toDate())
     }
