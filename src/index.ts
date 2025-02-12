23be928ae6e70d44e8ce24b05c13a115ee196868
pedrox10 <barcocarrasco.pedro@gmail.com>
2025-02-12 10:29:06 -0400

    Antes de presentacion

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,16 @@ const path = require("path");
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(cors());
 app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname, "../public")))
 app.use(routes)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
 
 app.get('/', (req, res) => {
     res.send("Inicio Server")
