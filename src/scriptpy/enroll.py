5bfecc4bfa341bd6f8432bc57c4db59a5f2e9f6a
pedrox10 <barcocarrasco.pedro@gmail.com>
2025-05-19 18:04:16 -0400

    desde work 19 May

--- src/scriptpy/enroll.py
+++ src/scriptpy/enroll.py
@@ -9,7 +9,6 @@ if len(sys.argv) < 3:
 ip = sys.argv[1]
 puerto = int(sys.argv[2])
 uid = int(sys.argv[3])
-
 zk = ZK(ip, port=puerto, timeout=10, force_udp=False, ommit_ping=False)
 try:
     conn = zk.connect()
@@ -18,7 +17,6 @@ try:
     print(f"Iniciando enrolamiento para UID {uid}... Coloque el dedo en el sensor")
     zk.enroll_user(uid=211, temp_id=0, user_id='5907490')
     print("Huella digital enrolada correctamente")
-
     conn.disconnect()
 except Exception as e:
     print(f"Error al enrolar usuario: {str(e)}")
