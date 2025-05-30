a356495b6d1cf6e34c6249c254750bd79a5417c2
pedrox10 <barcocarrasco.pedro@gmail.com>
2025-05-30 17:58:08 -0400

    Desde work 30 May

---
+++ src/entity/UsuarioLogin.ts
@@ -0,0 +1,15 @@
+import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity
+export class UsuarioLogin extends BaseEntity {
+    @PrimaryGeneratedColumn
+    id: number
+    @Column
+    nombre: string
+    @Column
+    nombreUsuario: string
+    @Column
+    clave: string
+    @Column({ default: 'admin' }) // valores: 'admin', 'visor'
+    rol: string;
+}
